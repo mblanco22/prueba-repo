3d8675577328815d7c41d00f39f0fa7bc075dd04
mblanco <mattosd45@yahoo.com.ar>
2022-11-01 21:02:07 -0300

    cambio en archivos

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,45 @@
 let nombre = "Matias";
+let apellido = "Blanco";
+let edad = "27 años";
+let mascotas = "Tengo 2 perros";
+
+let nombreApellido = nombre + " " + apellido;
+
+console.log(nombreApellido)
+
+let entrada = prompt("Ingrese su nombre");
+
+let salida = entrada + " " + "ingreso";
+
+alert(entrada)
+alert(salida)
+
+console.log(salida)
+
+/*let nombreIngresado = prompt("ingrese su nombre");*/
+
+/*alert("¡Hola Mundo!")*/
+
+
+
+/*console.log(typeof nombre)
+console.log(nombre)
+console.log(apellido)
+console.log(edad)
+console.log(mascotas)
+
+console.log(typeof numeroUno)
+console.log(numeroUno)
 
 let numeroUno = 5;
 
 const numeroDos = 10;
 
-console.log(nombre)
+let resultadoSuma = numeroUno + numeroDos;
+
+let resultadoResta = numeroUno - numeroDos;
+
+let resultadoMulti = numeroUno * numeroDos;
+
+let resultadoDiv = numeroUno / numeroDos;
+*/ 
