25e28a954ab6a607cfeaf284bbdc6a8176e3388a
mblanco <mattosd45@yahoo.com.ar>
2022-11-03 21:03:48 -0300

    cambio en archivos

--- javascript/index.js
+++ javascript/index.js
@@ -5,7 +5,7 @@ let mascotas = "Tengo 2 perros";
 
 let nombreApellido = nombre + " " + apellido;
 
-console.log(nombreApellido)
+/*console.log(nombreApellido)
 
 let entrada = prompt("Ingrese su nombre");
 
@@ -14,7 +14,7 @@ let salida = entrada + " " + "ingreso";
 alert(entrada)
 alert(salida)
 
-console.log(salida)
+console.log(salida)*/
 
 /*let nombreIngresado = prompt("ingrese su nombre");*/
 
@@ -43,3 +43,61 @@ let resultadoMulti = numeroUno * numeroDos;
 
 let resultadoDiv = numeroUno / numeroDos;
 */ 
+
+
+/*let entryNumber = prompt("Ingrese su edad");
+
+let edad = 18;
+
+let esMayorEdad = edad > 18 ? true : false;
+
+if(entryNumber > 18) {
+    alert("Pasa tranquilo")
+} else {
+    alert("No podes entrar son menor")
+}*/
+
+/*let entryNumber = prompt("Ingrese un numero");
+
+let arrayStrings = [
+    ["Perro", "Gato", "Pez", "Tortuga", "Pajaro"]
+]
+
+console.log(arrayStrings)
+
+console.log(arrayStrings.length)
+
+console.log(arrayStrings[5])*/
+
+/*let articleList = ["Leche", "Pan", "Carne"]
+
+let articleEntry = prompt("ingrese un articulo: ");
+
+if (!articleEntry) {
+
+    alert("No ah ingresado ningun articulo");
+    
+} else if (articleList.includes(articleEntry)) {
+
+    alert("El producto ya esta en la lista");
+
+} else {
+    articleList.push(articleEntry);
+    alert("La lista de productos contiene: " + articleList);
+    console.log(articleList)
+}*/
+
+/*let entryNumber = prompt("Ingrese un numero: ");
+
+if (entryNumber > 10) {
+    console.log("El valor ingresado es mayor o igual a 10")
+    document.write("<h1>El valor ingresado es mayor o igual a 10</h1>")
+
+} else if (entryNumber <= 10) { 
+    console.log("El valor ingresado es menor o igual a 10")
+    document.write("<h1>El valor ingresado es menor o igual a 10</h1>")
+
+} else {
+    console.log("El valor ingresado no es un numero")
+
+}*/
