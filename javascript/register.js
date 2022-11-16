1d10f62330e9866b40a869db412a41913fc31588
mblanco <mattosd45@yahoo.com.ar>
2022-11-15 21:07:30 -0300

    cambio en archivos

--- javascript/register.js
+++ javascript/register.js
@@ -3,12 +3,18 @@ console.log("success");
 let nombre = document.querySelector("#register-name");
 let apellido = document.querySelector("#register-surname");
 let email = document.querySelector("#register-email");
+let validRegex =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+ regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/,
+ regExEmail =
+    /^(([^<>()\[\]\.,;:\s@\”]+(\.[^<>()\[\]\.,;:\s@\”]:+)*)|(\”.+\”))@(([^<>()[\]\.,;:\s@\”]+\.)+[^<>()[\]\.,;:\s@\”]{2,})$/
+
 
 
 window.addEventListener("DOMContentLoaded", (e) => {
 
-    nombre.addEventListener("blur" , (e) => {
-        if(e.target && e.target.value.trim().length < 3) {
+    nombre.addEventListener("blur", (e) => {
+        if(e.target && e.target.value.trim().length < 3 || e.target.value.trim().length > 20) {
            nombre.style.backgroundColor = "red";
         } else {
             nombre.style.backgroundColor = "green";
@@ -16,3 +22,26 @@ window.addEventListener("DOMContentLoaded", (e) => {
     })
 
 });
+
+apellido.addEventListener("blur", (e) => {
+    if(e.target && e.target.value.trim().length < 3 || e.target.value.trim().length > 20) {
+       nombre.style.backgroundColor = "red";
+    } else {
+        nombre.style.backgroundColor = "green";
+    }
+});
+
+
+
+
+
+
+function Validate(input) {
+    if (input.value.match(validRegex)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
