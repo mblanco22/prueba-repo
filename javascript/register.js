edefdebb9e8b63cc410acde64c517051388fac7d
mblanco <mattosd45@yahoo.com.ar>
2022-11-21 20:31:25 -0300

    cambio en archivos

--- javascript/register.js
+++ javascript/register.js
@@ -1,89 +1,71 @@
-console.log("success");
+const nombre = document.getElementById("myname");
+const apellidos = document.getElementById("surname");
+const correo = document.getElementById("email");
+const celular = document.getElementById("mobile");
+const contrasenia = document.getElementById("password");
+const contrasenia2 = document.getElementById("repeatPassword");
+const terminosYcondiciones = document.getElementById("termsAndConditions");
+const form = document.getElementById("form");
+const listInputs = document.querySelectorAll(".form-input");
 
-let nombre = document.querySelector("#register-name");
-let apellido = document.querySelector("#register-surname");
-let email = document.querySelector("#register-email");
-let registerForm = document.querySelector("#register-form");
-let registerErrors = document.querySelector("#register-errors");
-let validRegex =
-  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-window.addEventListener("DOMContentLoaded", () => {
-  nombre.addEventListener("blur", (e) => {
-    listenNameAndSurname(nombre, e);
-  });
-
-  apellido.addEventListener("blur", (e) => {
-    listenNameAndSurname(apellido, e);
-  });
-
-  email.addEventListener("blur", (e) => {
-    listenerEmail(email, e);
-  });
-
-  registerForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let elements = event.target.elements;
-    let result = validateForm(event, elements);
-    !result && event.target.submit();
-  });
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let condicion = validacionForm();
+  if (condicion) {
+    enviarFormulario();
+  }
 });
 
-// FUNCIONES //
+function validacionForm() {
+  form.lastElementChild.innerHTML = "";
+  let condicion = true;
+  listInputs.forEach((element) => {
+    element.lastElementChild.innerHTML = "";
+  });
 
-let listenNameAndSurname = (input, e) => {
+  if (nombre.value.length < 1 || nombre.value.trim() == "") {
+    mostrarMensajeError("myname", "Nombre no valido*");
+    condicion = false;
+  }
+  if (apellidos.value.length < 1 || apellidos.value.trim() == "") {
+    mostrarMensajeError("surname", "Apellido no valido");
+    condicion = false;
+  }
+  if (correo.value.length < 1 || correo.value.trim() == "") {
+    mostrarMensajeError("email", "Correo no valido*");
+    condicion = false;
+  }
   if (
-    (e.target && e.target.value.trim().length < 3) ||
-    e.target.value.trim().length > 20
+    celular.value.length != 10 ||
+    celular.value.trim() == "" ||
+    isNaN(celular.value)
   ) {
-    input.classList.remove("border-success");
-    input.classList.add("border-danger");
-  } else {
-    input.classList.remove("border-danger");
-    input.classList.add("border-success");
+    mostrarMensajeError("mobile", "Celular no valido*");
+    condicion = false;
   }
-};
-
-let listenerEmail = (input, e) => {
-  if (
-    (e.target && e.target.value.trim().length < 3) ||
-    e.target.value.trim().length > 30 ||
-    !e.target.value.match(validRegex)
-  ) {
-    input.classList.remove("border-success");
-    input.classList.add("border-danger");
-  } else {
-    input.classList.remove("border-danger");
-    input.classList.add("border-success");
+  if (contrasenia.value.length < 1 || contrasenia.value.trim() == "") {
+    mostrarMensajeError("password", "Contraseña no valido*");
+    condicion = false;
   }
-};
-
-function validateEmail(input) {
-  if (input.value.match(validRegex)) {
-    return true;
-  } else {
-    return false;
+  if (contrasenia2.value != contrasenia.value) {
+    mostrarMensajeError("repeatPassword", "Contraseña error*");
+    condicion = false;
   }
+  if (!terminosYcondiciones.checked) {
+    mostrarMensajeError("termsAndConditions", "Acepte*");
+    condicion = false;
+  } else {
+    mostrarMensajeError("termsAndConditions", "");
+  }
+  return condicion;
 }
 
-function validateForm(event, elements) {
-  let errors;
-  if (event.target) {
-    for (let i = 0; i < elements.length - 6; i++) {
-      const element = elements[i];
-      if (!element.value || element.value == undefined) {
-        registerErrors.innerText = "Los campos señalados son obligatorios";
-        element.classList.remove("border-success");
-        element.classList.add("border-danger");
-        errors = true;
-      } else {
-        element.classList.remove("border-danger");
-        validateEmail(email)
-          ? element.classList.remove("border-danger")
-          : email.classList.add("border-danger");
-        errors = false;
-      }
-    }
-  }
-  return errors;
+function mostrarMensajeError(claseInput, mensaje) {
+  let elemento = document.querySelector(`.${claseInput}`);
+  elemento.lastElementChild.innerHTML = mensaje;
+}
+
+function enviarFormulario() {
+  form.reset();
+  form.lastElementChild.innerHTML = "Enviado !!";
 }
